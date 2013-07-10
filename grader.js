3001fe6d291a98ba876f2d0d8cad190e30b5c017
jeffrey-l-turner <jlt@cambix.org>
2013-07-10 19:18:00 +0000

    This is an intermediate edit for adding restler and URL checking

--- grader.js
+++ grader.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
+Uses commander.js and cheerio, and restler. Teaches command line application development
 and basic DOM parsing.
 
 References:
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
+var URLPATH_DEFAULT = "http://127.0.0.1//index.html"; // Defaults to localhost index.html
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -35,6 +36,16 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertURLExists = function(url) {
+    /*   PUT some code here to check if URL is valid
+	if(!fs.existsSync(instr)) {
+          console.log("%s does not exist. Exiting.", instr);
+          process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+*/
+    return url;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -56,6 +67,29 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+/* var buildfn = function(csvfile, headers) {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            console.error("Wrote %s", csvfile);
+            fs.writeFileSync(csvfile, result);
+            csv2console(csvfile, headers);
+        }
+    };
+    return response2console;
+};
+
+var marketResearch = function(symbols, columns, csvfile, headers) {
+    symbols = symbols || SYMBOLS_DEFAULT;
+    columns = columns || COLUMNS_DEFAULT;
+    csvfile = csvfile || CSVFILE_DEFAULT;
+    headers = headers || HEADERS_DEFAULT;
+    var apiurl = financeurl(symbols, columns);
+    var response2console = buildfn(csvfile, headers);
+    rest.get(apiurl).on('complete', response2console);
+}; */
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,6 +99,7 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to html file for grading', clone(assertURLExists), URLPATH_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);

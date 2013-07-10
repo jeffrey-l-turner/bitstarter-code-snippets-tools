bc5f4024656e6e5c2a2b08489a16b13056a7e153
jeffrey-l-turner <jlt@cambix.org>
2013-07-10 18:25:51 +0000

    a Please enter the commit message for your changes. Lines starting

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 

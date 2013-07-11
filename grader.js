af0a02c5954e5f0f8b397a90632660564011c250
jeffrey-l-turner <jlt@cambix.org>
2013-07-11 21:20:52 +0000

    updated to new buildFcn, restructured

--- grader.js
+++ grader.js
@@ -11,6 +11,9 @@ References:
    - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
    - http://maxogden.com/scraping-with-node.html
 
+ + restler
+   - https://github.com/danwrong/Restler/
+
  + commander.js
    - https://github.com/visionmedia/commander.js
    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
@@ -25,8 +28,8 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
-/* var URLPATH_DEFAULT = "http://127.0.0.1//index.html"; // Defaults to localhost index.html */
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -38,92 +41,34 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var assertURLExists = function(uri) {
-    /*   PUT some code here to check if URL is valid
-	if(!fs.existsSync(instr)) {
-          console.log("%s does not exist. Exiting.", instr);
-          process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-        
-	rest.get(url).on('complete', function(result) { 
+var buildFcn = function(jsoncheck, html) {
+    var response2 = function(result, response) {
         if (result instanceof Error) {
-                console.error('Error: ' + result.message);
-                process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-                } else {
-                return url;
-       }
-    }
-*/
-    url = true;
-    return uri;
-};
+            console.error('Error: ' + util.format(response.message));
+        } else  {
+            var checks = loadChecks(jsoncheck).sort();
+            $ = cheerio.load(result);
+            var out = {};
+            for(var ii in checks) {
+                var present = $(checks[ii]).length > 0;
+                out[checks[ii]] = present;
+            }
 
-var callback = function(rslt) {
-    if (rslt instanceof Error) {
-        console.error('Error: ' + rslt.message);
-        process.exit(1);                          // http://nodejs.org/api/process.html#process_process_exit_code
-    } else {
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-    }
-};
-
-var cheerioHTML = function(html) {
-/* add if statement to return load dependent on if HTML or a file */
-    if (url === true) {
-	rest.get(html).on('complete', callback(dom));
+            var outJson = JSON.stringify(out, null, 4);
+            console.error("Got %s for html", html);
+            console.log(outJson);
         }
-    else return cheerio.load(fs.readFile(html, callback(dom)));
+    };
+    return response2;
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHTML = function(html, checksfile) {
-    console.log("html = " + html);
-    $ = cheerioHTML(html);
-//  console.log("$ = " + $);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+var assertURLExists = function(url) {
+    return url;
 };
-
-/* var buildfn = function(csvfile, headers) {
-    var response2console = function(result, response) {
-        if (result instanceof Error) {
-            console.error('Error: ' + util.format(response.message));
-        } else {
-            console.error("Wrote %s", csvfile);
-            fs.writeFileSync(csvfile, result);
-            csv2console(csvfile, headers);
-        }
-    };
-    return response2console;
-};
-
-var marketResearch = function(symbols, columns, csvfile, headers) {
-    symbols = symbols || SYMBOLS_DEFAULT;
-    columns = columns || COLUMNS_DEFAULT;
-    csvfile = csvfile || CSVFILE_DEFAULT;
-    headers = headers || HEADERS_DEFAULT;
-    var apiurl = financeurl(symbols, columns);
-    var response2console = buildfn(csvfile, headers);
-    rest.get(apiurl).on('complete', response2console);
-};
-
-rest.get('http://google.com').on('complete', function(result) { 
-  if (result instanceof Error) {
-    console.error('Error: ' + result.message);
-  } else {
-    return result;
-  }
-
-
-*/
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -131,8 +76,7 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-var url = false;
-var dom;
+var url = true;
 
 if(require.main == module) {
     program
@@ -140,12 +84,20 @@ if(require.main == module) {
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-u, --url <url>', 'URL to html file for grading', clone(assertURLExists))
         .parse(process.argv);
-    if (program.url) // { console.log("program.url = " + program.url); 
-                       //          console.log("program.checks = " + program.checks); 
-              { var checkJson = checkHTML(program.url, program.checks); } 
-    else { url = false; var checkJson = checkHTML(program.file, program.checks);}
-//    var outJson = JSON.stringify(checkJson, null, 4);
-//    console.log(outJson);
+    if (program.url) {
+          var chksfile = program.checks;
+          var html = program.url;
+          var httpGetResponse = buildFcn(chksfile, html);
+          console.error("chksfile =" + chksfile + " ; html = " + html);
+          rest.get(html).on('complete', httpGetResponse);
+          }
+    else { url = false;
+          var chksfile = program.checks;
+          var htmlfile = program.file;
+          console.error("chksfile =" + chksfile + " ; html = " + html);
+          var fsReadResponse = buildFcn(chksfile, html);
+          fs.readFile(htmlfile, fsReadResponse);
+         }
 } else {
-    exports.checkHtml = checkHTML;
+    exports.checkHtmlFile = buildFcn;
 }

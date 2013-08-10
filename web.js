e7ee3f61eb833c58f4cbe893c80b350ff3153573
jeffrey-l-turner <jeffrey.l.turner@me.com>
2013-08-10 03:58:33 +0000

    updated web.js to remove diff changes

--- web.js
+++ web.js
@@ -113,25 +113,3 @@ var addOrder = function(order_obj, callback) {
     });
   }
 };
-=======
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-<<<<<<< HEAD
-  response.send('Hello World2!');
-=======
-  response.send('Hello World!');
->>>>>>> 950a10a7020a48cbdbf62b1311f5dd0c768c3675
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-<<<<<<< HEAD
-});
-=======
-});
->>>>>>> 950a10a7020a48cbdbf62b1311f5dd0c768c3675
->>>>>>> 616fad4154c46adef0f5f5a5b008c775e9288a27
